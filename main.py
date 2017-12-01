2be60bb82422166e25857add91bcc689a14024d2
Benjamin Slack <iam@nimajneb.com>
2017-12-01 17:09:06 -0500

    Added comments with concerns.

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ if __name__ == "__main__":
         value = input("Please input the tape you want to use?")
         #value = "ababba"
         #print(tm.tapealpha)
+
+        # Benjamin A. Slack: 12.1.17
+        # Why are you checking the input twice?
+        # You are attempting to check it here with
+        # the if statement. Then you check it again
+        # on load. Either or would do.
+        #
+        # If you're checking it here, you need to make a
+        # set from the input, not a list. Example:
+        # set(value)
+        # read your docs:
+        # https://docs.python.org/3/tutorial/datastructures.html#sets
+        # you don't even need to give it a name. Just
+        # call the methods on the object at creation time
+        # Example
+        # if (set(value).isSubset(tm.alpha)):
+        # ...
+
         valuelist = list(value)
         #print (valuelist)
         fox = tm.alpha
@@ -29,6 +47,13 @@ if __name__ == "__main__":
                     elif 'Accepted:' in config:
                         print("Congratulations: " + value + " was ACCEPTED.")
 
+            # Benjamin A. Slack : 12.1.17
+            # if you're going to handle an exception,
+            # you should use the info the exception gives you
+            # Example:
+            # except machine.InvalidCharacterInTape as e:
+            #     print(type(e), *e.args)
+            # this would print out what went wrong and why
             except:
                 print("Invalid input for machine!")
 
